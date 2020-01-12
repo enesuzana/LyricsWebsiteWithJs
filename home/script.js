f0c0df8d0d91ba678a245a77bf3b4e781c440ce0
Suzi <suzana_ene@yahoo.com>
2020-01-12 19:55:41 +0200

    animations js

--- home/script.js
+++ home/script.js
@@ -1,11 +1,4 @@
-window.onload = function() {
-    submit.setAttribute("style", "opacity: 0.5;");
-    submit.setAttribute('disabled', 'disabled');
-    checkbox.checked = false;
-    submit.onmouseover = function() {
-        submit.style.cursor = "not-allowed";
-    };
-}
+// Script
 
 function createElement(tagName, attrs, appendTo) {
     var element = document.createElement(tagName);
@@ -28,6 +21,64 @@ function createElement(tagName, attrs, appendTo) {
     if (appendTo) appendTo.appendChild(element);
     return element;
 }
+
+var hex1 = 239;
+var hex2 = 194;
+var hex3 = 224;
+var headerText = document.getElementsByClassName('header')[0].getElementsByTagName('h1')[0];
+headerText.style.textShadow = "1px 1px 1px #391326";
+headerText.setAttribute('style', 'animation:pop 1s ease-in-out;');
+
+function fadetext() {
+    if (hex1 > 182) { //If color is not black yet
+        hex1 -= 5; // increase color darkness
+        hex2 -= 20;
+        hex3 -= 11;
+        headerText.style.color = "rgb(" + hex1 + "," + hex2 + "," + hex3 + ")";
+        setTimeout("fadetext()", 80);
+    } else {
+        hex1 = 224;
+        hex2 = 133;
+        hex3 = 194;
+    }
+}
+
+window.onload = function() {
+    submit.setAttribute("style", "opacity: 0.5;");
+    submit.setAttribute('disabled', 'disabled');
+    checkbox.checked = false;
+    submit.onmouseover = function() {
+        submit.style.cursor = "not-allowed";
+    };
+
+    // set a random image as header image 
+    document.getElementsByClassName('header')[0].setAttribute('id', 'header');
+    var header = document.getElementById('header');
+    var pictures = new Array('https://i.pinimg.com/originals/8b/61/c1/8b61c154e7f44146727a32c05a7539d9.gif',
+        'https://i.pinimg.com/originals/4c/8e/26/4c8e267ee4446e733bb17564337083f7.jpg',
+        'https://i0.wp.com/windowscustomization.com/wp-content/uploads/2018/10/cyberpunk-night-city.gif?fit=750%2C364&quality=80&strip=all&ssl=1',
+        'http://s13.favim.com/orig/160531/anime-scenery-gif-anime-gifs-Favim.com-4364443.gif',
+        'https://i.pinimg.com/originals/76/90/8b/76908b09072332bd62e7cf92b3042dd2.gif',
+        'https://data.whicdn.com/images/319359512/original.gif',
+        'https://i.pinimg.com/originals/05/59/3e/05593e925e651314820bbb215b223a5f.jpg'
+    );
+    var numPics = pictures.length;
+    if (document.images) {
+        var chosenPic = Math.floor((Math.random() * numPics));
+        header.style.backgroundSize = 'cover';
+        header.style.backgroundRepeat = 'no-repeat';
+        header.style.backgroundPosition = 'center center';
+        header.style.backgroundImage = 'url(' + pictures[chosenPic] + ')';
+    }
+    var div = document.createElement('div');
+    div.setAttribute('class', 'popText');
+    header.appendChild(div);
+    div.setAttribute('style', 'animation:pop 1s ease-in-out; animation-delay:0.2s;');
+    div.style.textShadow = "5px 5px 1px #9900ff, 10px 10px 1px #ff4d94";
+    div.appendChild(document.getElementsByTagName('p')[0]);
+    fadetext();
+}
+
 
 // Create form element
 var form = createElement('form', {
@@ -229,102 +280,24 @@ checkbox.addEventListener('change', function() {
     }
 })
 
-// Form validation
-
-function checkForm(form) {
-    // validate username
-    if (!validateUsername(username)) {
-        return false;
-    }
-
-    // validate email
-    if (!validateEmail(email)) {
-        return false;
-    }
-    // validate password
-    if (!validatePassword(password)) {
-        return false;
-    }
-    // validate checkbox
-    if (!validateCheckbox) {
-        return false;
-    }
-    alert('Good!');
-    return true;
-}
-
-submit.addEventListener('click', function() {
-    if (!checkForm(form)) {
-
-    }
-})
-
 // VALIDATIONS
 
-// Validate username
-function validateUsername(username) {
-    if (username.value == "") {
-        alert("Error: Username is empty!");
-        username.focus();
+function validateInput(element, format) {
+    if (element.value == "") {
+        alert("Error:" + toString(element.type) + " is empty!");
+        element.focus();
         return false;
     }
 
-    const usernameFormat = /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
-
-    if (username.value.match(usernameFormat)) {
-        username.focus();
+    if (element.value.match(format)) {
+        element.focus();
+        element.setAttribute("style", "border: 2px solid green;");
         return true;
     }
-    email.focus();
+    element.focus();
+    element.setAttribute("style", "border: 1px solid red;");
     return false;
 }
-
-// Validate email
-function validateEmail(email) {
-    if (email.value == "") {
-        alert("Error: Email is empty!");
-        email.focus();
-        return false;
-    }
-
-    const mailformat = /^[a-zA-Z0-9!#$&_*?^{}~-]+(\.[a-zA-Z0-9!#$&_*?^{}~-]+)*@([a-z0-9]+([a-z0-9]*)\.)+[a-zA-Z]+$/;
-
-    if (email.value.match(mailformat)) {
-        email.focus();
-        return true;
-    }
-    email.focus();
-    return false;
-}
-
-
-email.oninput = function() {
-    validateEmail(this);
-};
-
-function validatePassword(password) {
-    if (password.value == "") {
-        alert("Error: Password is empty!");
-        this.focus();
-        return false;
-    }
-
-    const passwordFormat = /(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!#$&_*?^{}~-])[a-zA-Z!#$&_*?^{}~-]{8,}/;
-
-    if (password.value.match(passwordFormat)) {
-        console.log("yay");
-        this.focus();
-        return true;
-    }
-
-    this.focus();
-    return false;
-}
-
-password.oninput = function() {
-    validatePassword(this);
-}
-
 // User type validation
 
 usertype.onchange = function() {
@@ -343,14 +316,23 @@ function validateCheckbox() {
     return false;
 }
 
-radio.onchange = function validateRadioButton() {
-    if (radio = checkRadio(form.radiofield)) {
-        alert("You selected " + radioValue);
+
+var checkRadio = false;
+
+function uncheckRadio(radio) {
+    radio.checked = false;
+}
+
+radio.addEventListener('click', function() {
+    if (this.checked && !checkRadio) {
+        alert("You selected premium");
+        checkRadio = true;
         return true;
     }
-    alert("Error: No value was selected!");
+    uncheckRadio(radio);
+    checkRadio = false;
     return false;
-}
+});
 
 const checkSelect = (field) => {
     let retval = [];
@@ -362,3 +344,51 @@ const checkSelect = (field) => {
     }
     return (field.multiple) ? retval : retval[0];
 };
+
+// Form validation
+const emailFormat = /^[a-zA-Z0-9!#$&_*?^{}~-]+(\.[a-zA-Z0-9!#$&_*?^{}~-]+)*@([a-z0-9]+([a-z0-9]*)\.)+[a-zA-Z]+$/;
+const usernameFormat = /^[a-zA-Z0-9_]{5,}[a-zA-Z]+[0-9]*$/;
+const passwordFormat = /(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!#$&_*?^{}~-])[a-zA-Z!#$&_*?^{}~-]{8,}/;
+
+email.oninput = function() {
+    validateInput(this, emailFormat);
+};
+
+username.oninput = function() {
+    validateInput(this, usernameFormat);
+}
+
+password.oninput = function() {
+    validateInput(this, passwordFormat);
+}
+
+function checkForm(form) {
+    // validate email
+    if (!validateInput(email, emailFormat)) {
+        console.log("Email invalid");
+        return false;
+    }
+    // validate username
+    if (!validateInput(username, usernameFormat)) {
+        console.log("Username invalid");
+        return false;
+    }
+    // validate password
+    if (!validateInput(password, passwordFormat)) {
+        console.log("Pass invalid");
+        return false;
+    }
+    // validate checkbox
+    if (!validateCheckbox) {
+        return false;
+    }
+    alert('Good!');
+    return true;
+}
+
+submit.addEventListener('click', function() {
+    if (checkForm(form)) {
+        alert('congrats, checkfrom corect');
+        document.getElementsByClassName('gallery')[0].setAttribute("style", "display:none;");
+    }
+})
